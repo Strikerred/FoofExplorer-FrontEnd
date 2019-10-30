4181480a60ccfe0cbc5f3d880598a7ed8476d8f3
Crystal McLean <cmclean48@my.bcit.ca>
2019-10-30 10:03:22 -0700

    new js file

--- dev/scripts/myproject.js
+++ dev/scripts/myproject.js
@@ -1,6 +1,56 @@
+
+
+//JavaScript Document
+
+// Responsive Menu - Dropdown
 const body = document.body;
-const btn = document.getElementById('btn-menu');
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
 
-btn.addEventListener('click', _=>body.classList.toggle('show'));
+// Below code for preventing nav from animating on 
+// browser re-size modified from code found at
+// this stackoverflow question and answer:
+//
+// https://goo.gl/6s3pAZ
 
-btn.addEventListener('mousedown', (e) => {e.preventDefault()});
+
+btnMenu.addEventListener('click', openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener('mousedown', function(e){
+    e.preventDefault();
+});
+
+function openMenu(){
+    body.classList.toggle('show');
+	nav.classList.add('activated');
+}
+
+// Media Query Event Listener
+// - This is used to remove the "activated"
+// class from the navigation when the user
+// resizes the browser
+
+// Create a media query list using
+// matchMedia
+const mql = window.matchMedia('(min-width: 560px)');
+
+// Add a Media Query Listener to the 
+// above media query list
+mql.addListener(removeTransition);
+
+// Function to remove the transition
+// from the navigation when the user
+// resizes the browser to desktop
+// sizes. In this case when the
+// screen width becomes wider then
+// 560px
+function removeTransition(e){
+	// e -> is the event object
+	// e.matches -> stores a true false
+	// value depending if the media query
+	// list set above matches or not
+	if(e.matches){
+		body.classList.remove('show');
+		nav.classList.remove('activated');
+	}
+}
